c674c0f11eaa05314d4be431570eab93d1926dca
Dasol (David) Lim <92288227+DasolLim@users.noreply.github.com>
2025-01-21 22:41:29 -0500

    Implement user profile picture upload directory

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
 
-const express = require("express")
-const app = express()
+const authenticateToken = require("./Middleware/JwtAuth.js");
+const { Logger } = require("./Middleware/Logger.js");
+require("dotenv").config();
 
-const authenticateToken = require("./Middleware/JwtAuth.js")
-const { Logger } = require("./Middleware/Logger.js")
-require('dotenv').config();
+const userRoutes = require("./Routes/UserRoutes.js");
 
-const userRoutes = require("./Routes/UserRoutes.js")
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(Logger);
 
+// Static route to serve uploaded files
+const uploadsDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+}
+app.use("/uploads", express.static(uploadsDir));
 
-//Middleware 
-app.use(express.json()); //parse JSON Data
-app.use(Logger)
-
-//Routes
-app.use("/users", userRoutes)
+app.use("/users", userRoutes);
 app.use("/dashboard", authenticateToken);
 
-
-
 // Start the server
-// Modify the ip address to start the server, ensure ip enabled with MongoDB
-app.listen(3000, '192.168.2.19', async () => {  // ipv4
-    console.log('Server running on port 3000');
+app.listen(3000, "192.168.2.19", () => {
+    console.log("Server running on http://192.168.2.19:3000");
 });
